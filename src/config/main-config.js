b53d7845b047cec55fea6dfd76a1b9e111e20d9a
maiphutri <mp.ritz@yahoo.com.vn>
2019-04-11 16:43:18 -0700

    fix

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 const path       = require("path"),
-      viewFolder = path.join(__dirname, "..", "views"),
-      logger     = require("morgan");
+      viewFolder = path.join(__dirname, "..", "views");
 
 module.exports = {
   init(app, express) {
     app.set("views", viewFolder);
     app.set("view engine", "ejs");
     app.use(express.static(path.join(__dirname, "..", "assets")));
-    app.use(logger('dev'));
   }
 }
