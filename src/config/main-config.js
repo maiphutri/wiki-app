683d344c15004dc2d7fed166b31b5b3f775231d8
maiphutri <mp.ritz@yahoo.com.vn>
2019-04-11 15:56:33 -0700

    Scaffolding & Deployment: Completed

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-const path = require("path"),
-      viewFolder = path.join(__dirname, "..", "views");
+const path       = require("path"),
+      viewFolder = path.join(__dirname, "..", "views"),
+      logger     = require("morgan");
 
 module.exports = {
   init(app, express) {
     app.set("views", viewFolder);
     app.set("view engine", "ejs");
     app.use(express.static(path.join(__dirname, "..", "assets")));
+    app.use(logger('dev'));
   }
 }
