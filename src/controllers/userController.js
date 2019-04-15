cb5e7ff22b6f18dbe0a66ed42d2e0db8a081e958
maiphutri <mp.ritz@yahoo.com.vn>
2019-04-14 20:07:43 -0700

    Fix syntax error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,19 +21,18 @@ module.exports = {
         res.redirect("/users/sign_up");
       } else {
         passport.authenticate("local")(req, res, () => {
-          const helper = require('sendgrid').mail,
-          var from_email = new helper.Email('blocipedia@wiki.com');
-          var to_email = new helper.Email(newUser.email);
-          var subject = 'Welcome to Blocipedia! Confirm your email.';
-          var content = new helper.Content('text/plain', 'Hello, Please Confirm You Email!');
-          var mail = new helper.Mail(from_email, subject, to_email, content);
-
-          var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-          var request = sg.emptyRequest({
-            method: 'POST',
-            path: '/v3/mail/send',
-            body: mail.toJSON(),
-          });
+          const helper     = require('sendgrid').mail,
+                from_email = new helper.Email('blocipedia@wiki.com'),
+                to_email = new helper.Email(newUser.email),
+                subject = 'Welcome to Blocipedia! Confirm your email.',
+                content = new helper.Content('text/plain', 'Hello, Please Confirm You Email!'),
+                mail = new helper.Mail(from_email, subject, to_email, content),
+                sg = require('sendgrid')(process.env.SENDGRID_API_KEY),
+                request = sg.emptyRequest({
+                  method: 'POST',
+                  path: '/v3/mail/send',
+                  body: mail.toJSON(),
+                });
 
           sg.API(request, function(error, response) {
             console.log(response.statusCode);
